18c2ff2aec49a595476c719e64e874e7a4b95461
EpicEricMao <47091999+EpicEricMao@users.noreply.github.com>
2020-04-20 21:08:34 +0800

    Create vlist.js

---
+++ src/vlist.js
@@ -0,0 +1,8 @@
+const vlist = [
+  "QMfk4zc4PiU", "ug_0Xna2_x4",
+  "otMpU0P73Qw", "BD5HijbYsBY", "3atNbNXVFeM", "_yO9JaD6ruM", "9gnlM0d7cz4", "4cb2QwLTTIA", "5ikAG_Fn8hc", "Jn5_QXhDDkU",
+  "Vvg6aXq738s", "SKHWNaG-vgI", "N5dk6uW9fQA", "olsituXMj_g", "Mcmedfw7boI", "zzxfyjpqmGw", "KluCpTnpjfs", "WoHW-OkMpM0",
+  "g7-d83ZWjXY", "812tRKQ2vrU", "Su_mmGxAHGA", "sEdRPqPXEXg", "xN9BR0HtxbY", "7KfvMNLcZ8A", "mebGZcSr8hs", "FUCKRzch6As",
+  "hG7-XAL9pvg", "nRIwQe6UvGM", "r6qSWnLrkwE", "rXNjfXYo5u0", "ZlDxwqEDTuI", "JrpjGzkM-5M", "KIm2aDgrDFk", "pIN3gdsF4Ho",
+  "2hMX9Q1jpJ4", "JcxWFGLJ4YU", "KiqlTPJh3bc", "q7lGSuJKytI", "-0xcKzmmp08", "ACv7G2SLBn0", "61Xf_nNZsuo", "DLK2vQ2Wb2A"
+];
