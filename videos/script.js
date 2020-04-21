073d01810eb065d56904a2f8eb29b698f15080e8
Dogeon188 <dogcatdog188@gmail.com>
2020-04-21 20:46:18 +0800

    Update script.js

--- videos/script.js
+++ videos/script.js
@@ -1,21 +1,31 @@
+l = "https://spreadsheets.google.com/feeds/cells/1EAGFi2FniYnXfFhzBPYHRCYKfYdhKvtShUYSOAbzUvw/1/public/values?alt=json"
+
 String.prototype.f = function() {
-  var s = this,
-      i = arguments.length;
-  while (i--) {
-    s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
-  }
+  var s = this, i = arguments.length;
+  while (i--) {s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);}
   return s;
 };
 
-var videos = document.getElementById("videos");
-for (vid of vlist) {
-  let vth = document.createElement("IMG"),
-      thsrc = "https://img.youtube.com/vi/{0}/mqdefault.jpg".f(vid),
-      vlink = "https://youtu.be/{0}".f(vid),
-      va = document.createElement("A");
-  vth.setAttribute("src", thsrc);
-  vth.setAttribute("class", "video-thumbnail");
-  va.setAttribute("href", vlink);
-  va.appendChild(vth);
-  videos.appendChild(va);
-};
+function Get(u) {
+  var r = new XMLHttpRequest();
+  r.open("GET", u, false);
+  r.send(null);
+  return r.responseText;
+}
+
+var d = document.getElementById("videos");
+var j = JSON.parse(Get(l)).feed.entry;
+
+for (o of j) {
+  i = o.gs$cell.$t;
+  t = document.createElement("IMG");
+  s = "https://img.youtube.com/vi/{0}/mqdefault.jpg".f(i);
+  h = "https://youtu.be/{0}".f(i);
+  a = document.createElement("A");
+  if (i.slice(0,2) == "//") {continue; }
+  t.setAttribute("src", s);
+  t.setAttribute("class", "video-thumbnail");
+  a.setAttribute("href", h);
+  a.appendChild(t);
+  d.appendChild(a);
+}
