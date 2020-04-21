9741c1b0ce87cb7c5a97386ac641213d05192a0b
Dogeon188 <dogcatdog188@gmail.com>
2020-04-21 19:38:33 +0800

    Update script.js

--- videos/script.js
+++ videos/script.js
@@ -1,18 +1,20 @@
-String.prototype.format = String.prototype.f = function() {
-  var s = this, i = arguments.length;
-  while (i--) {s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);}
+String.prototype.f = function() {
+  var s = this,
+      i = arguments.length;
+  while (i--) {
+    s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+  }
   return s;
 };
 
 var videos = document.getElementById("videos");
 for (vid of vlist) {
-  var vth = document.createElement("IMG");
-  var thsrc = "https://img.youtube.com/vi/{0}/mqdefault.jpg".f(vid);
-  var vlink = "https://youtu.be/{0}".f(vid);
+  let vth = document.createElement("IMG"),
+      thsrc = "https://img.youtube.com/vi/{0}/mqdefault.jpg".f(vid),
+      vlink = "https://youtu.be/{0}".f(vid),
+      va = document.createElement("A");
   vth.setAttribute("src", thsrc);
-  vth.setAttribute("margin", 2);
-  vth.setAttribute("width", "calc(50% - 4px)");
-  var va = document.createElement("A");
+  vth.setAttribute("class", "video-thumbnail");
   va.setAttribute("href", vlink);
   va.appendChild(vth);
   videos.appendChild(va);
