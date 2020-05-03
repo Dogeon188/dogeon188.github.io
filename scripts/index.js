73e4f9b5e185460461ac233264186153f40784d5
Dogeon188 <dogcatdog188@gmail.com>
2020-05-03 17:27:14 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,4 @@
-function g(u) {
-  var r = new XMLHttpRequest();
-  r.open("GET", u, false);
-  r.send(null);
-  return r.responseText;
-}
+l = "https://spreadsheets.google.com/feeds/cells/1EAGFi2FniYnXfFhzBPYHRCYKfYdhKvtShUYSOAbzUvw/1/public/values?alt=json"
 
 String.prototype.f = function() {
   var s = this, i = arguments.length;
@@ -11,26 +6,50 @@ String.prototype.f = function() {
   return s;
 };
 
-for (t of JSON.parse(g("https://www.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=UCapFOd5gDDMNszKmVPKo9JQ&key=AIzaSyBbBboaavWuf5jiImHewI6vrXeJuHpdYCs&maxResults=2")).items) {
-  d = document.createElement("DIV");
-  s = document.createElement("SPAN");
-  s.appendChild(document.createTextNode(t.snippet.title));
-  d.appendChild(s);
-  i = document.createElement("IMG");
-  i.setAttribute("src", t.snippet.thumbnails.medium.url);
-  a = document.createElement("A");
-  a.setAttribute("href", "https://www.youtube.com/playlist?list={0}".f(t.id))
-  a.appendChild(i);
-  d.appendChild(a);
-  document.getElementById("plists").appendChild(d);
+function G(u) {
+  var r = new XMLHttpRequest();
+  r.open("GET", u, false);
+  r.send(null);
+  return r.responseText;
 }
 
-for (t of JSON.parse(g("https://www.googleapis.com/youtube/v3/search?key=AIzaSyBbBboaavWuf5jiImHewI6vrXeJuHpdYCs&channelId=UCapFOd5gDDMNszKmVPKo9JQ&part=snippet,id&order=date&maxResults=3")).items) {
+var d = document.getElementById("videos");
+var j = JSON.parse(G(l)).feed.entry;
+var c = 0;
+
+for (o of j) {
+  if (c >= 3) {break;}
+  i = o.gs$cell.$t;
+  if (i.slice(0,2) == "//") {continue; }
+  t = document.createElement("IMG");
   a = document.createElement("A");
-  a.setAttribute("href", t.id.videoId);
-  a.setAttribute("title", t.snippet.title);
-  i = document.createElement("IMG");
-  i.setAttribute("src", t.snippet.thumbnails.medium.url)
-  a.appendChild(i);
-  document.getElementById("vids").appendChild(a);
+  t.setAttribute("src", "https://img.youtube.com/vi/{0}/mqdefault.jpg".f(i));
+  t.setAttribute("class", "video-thumbnail");
+  a.setAttribute("href", "https://youtu.be/{0}".f(i));
+  a.appendChild(t);
+  d.appendChild(a);
+  c += 1;
 }
+
+// function g(u) {
+//   var r = new XMLHttpRequest();
+//   r.open("GET", u, false);
+//   r.send(null);
+//   return r.responseText;
+// }
+
+// String.prototype.f = function() {
+//   var s = this, i = arguments.length;
+//   while (i--) {s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);}
+//   return s;
+// };
+
+// for (t of JSON.parse(g("https://www.googleapis.com/youtube/v3/search?key=AIzaSyBbBboaavWuf5jiImHewI6vrXeJuHpdYCs&channelId=UCapFOd5gDDMNszKmVPKo9JQ&part=snippet,id&order=date&maxResults=3")).items) {
+//   a = document.createElement("A");
+//   a.setAttribute("href", t.id.videoId);
+//   a.setAttribute("title", t.snippet.title);
+//   i = document.createElement("IMG");
+//   i.setAttribute("src", t.snippet.thumbnails.medium.url)
+//   a.appendChild(i);
+//   document.getElementById("vids").appendChild(a);
+// }
