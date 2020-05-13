26366777bb5697e5a50de50f7b002ba6f6320ed4
Dogeon188 <dogcatdog188@gmail.com>
2020-05-13 09:32:28 +0800

    Create util.js

---
+++ scripts/util.js
@@ -0,0 +1,12 @@
+String.prototype.f = String.prototype.format = function() {
+  var s = this, i = arguments.length;
+  while (i--) {s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);}
+  return s;
+};
+
+function G(u) {
+  r = new XMLHttpRequest();
+  r.open("GET", u, false);
+  r.send(null);
+  return r.responseText;
+}
