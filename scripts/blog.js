2a5516b698ff0a9ffa112ea5a0386a436d7d2682
Dogeon188 <dogcatdog188@gmail.com>
2020-06-07 00:15:21 +0800

    owo

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,6 +1,7 @@
 l = "https://spreadsheets.google.com/feeds/cells/12kGrnf4m7rVOB3G-QDAU8UfhMSkSbxzEdLsOm7Pcm4E/1/public/values?alt=json";
 
 c = t => document.createElement(t);
+ac = (p,c) => p.appendChild(c);
 
 d = document.getElementById("blogs");
 j = JSON.parse(G(l)).feed.entry;
@@ -16,14 +17,16 @@ for (o of j) {
       v.setAttribute("class", "blog-post");
       h = c("H4");
       h.innerHTML = i.$t;
-      v.appendChild(h);
-      a.appendChild(v);
-      d.appendChild(a);
+      ac(v,h); ac(a,v); ac(d,a);
+      // v.appendChild(h);
+      // a.appendChild(v);
+      // d.appendChild(a);
       break;
     case '2':
       d.lastChild.setAttribute("href", i.$t);
       break;
     case '3':
+      ac(d.lastChild.firstChild,document.createTextNode(i.$t));
       d.lastChild.firstChild.appendChild(document.createTextNode(i.$t));
       break;
   }
--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ l = "https://spreadsheets.google.com/feeds/cells/{0}/1/public/values?alt=json"
 l1 = "1EAGFi2FniYnXfFhzBPYHRCYKfYdhKvtShUYSOAbzUvw"
 l2 = "12kGrnf4m7rVOB3G-QDAU8UfhMSkSbxzEdLsOm7Pcm4E"
 
-const ce = t => document.createElement(t);
-const ac = (p,c) => p.appendChild(c)
+ce = t => document.createElement(t);
+ac = (p,c) => p.appendChild(c);
 
 d = document.getElementById("vids");
 j = JSON.parse(G(l.f(l1))).feed.entry;
@@ -19,10 +19,7 @@ for (o of j) {
   t.setAttribute("src", "https://img.youtube.com/vi/{0}/mqdefault.jpg".f(i));
   t.setAttribute("class", "video-thumbnail");
   a.setAttribute("href", "https://youtu.be/{0}".f(i));
-  // a.appendChild(t);
-  // d.appendChild(a);
-  ac(a,t);
-  ac(d,a);
+  ac(a,t); ac(d,a);
   c++;
 }
 
@@ -42,19 +39,13 @@ for (o of j) {
       tdiv.setAttribute("class", "blog-post");
       th = ce("H4");
       th.innerHTML = i.$t;
-      // tdiv.appendChild(th);
-      // ta.appendChild(tdiv);
-      // d.appendChild(ta);
-      ac(tdiv,th);
-      ac(ta,tdiv);
-      ac(d,ta);
+      ac(tdiv,th); ac(ta,tdiv); ac(d,ta);
       break;
     case '2':
       d.lastChild.setAttribute("href", i.$t);
       break;
     case '3':
       tc = document.createTextNode(i.$t);
-      // d.lastChild.firstChild.appendChild(tc);
       ac(d.lastChild.firstChild,tc);
       break;
   }
