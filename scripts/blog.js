d4ff6a96343bcdc90e646c2d21ff294e816a7814
Dogeon188 <dogcatdog188@gmail.com>
2020-06-08 20:35:14 +0800

    Update blog.js

--- scripts/blog.js
+++ scripts/blog.js
@@ -10,20 +10,12 @@ for (o of j) {
   switch (i.col) {
     case '1':
       d.append($(`<a><div class="blog-post"><h4>${i.$t}</h4></div></a>`));
-      // a = $ce("A");
-      // v = $ce("DIV");
-      // v.class = "blog-post";
-      // h = $ce("H4");
-      // h.innerHTML = i.$t;
-      // $ac(v,h); $ac(a,v); $ac(d,a);
       break;
     case '2':
-      // d.lastChild.href = i.$t;
-      d.children().last().children().first().attr('href', i.$t);
+      d.children().last().attr('href', i.$t);
       break;
     case '3':
       d.children().last().children().first().append(i.$t);
-      // $ac(d.lastChild.firstChild,$ct(i.$t));
       break;
   }
 }
