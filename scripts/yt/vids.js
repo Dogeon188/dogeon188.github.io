8799697d3e50e01c23b8a1de2866fc712ca1af61
Dogeon188 <dogcatdog188@gmail.com>
2020-06-08 20:38:31 +0800

    Update vids.js

--- scripts/yt/vids.js
+++ scripts/yt/vids.js
@@ -1,16 +1,11 @@
 l = "https://spreadsheets.google.com/feeds/cells/1EAGFi2FniYnXfFhzBPYHRCYKfYdhKvtShUYSOAbzUvw/1/public/values?alt=json"
 
-var d = $$id("videos");
-d.innerHTML = "";
-var j = JSON.parse(G(l)).feed.entry;
+d = $("#videos");
+d.empty();
+j = JSON.parse(G(l)).feed.entry;
 
 for (o of j) {
   i = o.gs$cell.$t;
   if (i.slice(0,2) == "//") {continue; }
-  t = $ce("IMG");
-  a = $ce("A");
-  t.setAttribute("src", "https://img.youtube.com/vi/{0}/mqdefault.jpg".f(i));
-  t.setAttribute("class", "video-thumbnail");
-  a.setAttribute("href", "https://youtu.be/{0}".f(i));
-  $ac(a,t); $ac(d,a);
+  d.append($(`<a href="https://youtu.be/${i}"><img class="video-thumbnail" src="https://img.youtube.com/vi/${i}/mqdefault.jpg"></img></a>`));
 }
