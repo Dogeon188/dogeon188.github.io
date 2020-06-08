2df5954467c5ae7dfef9c9d69ec35eefff4a72d0
Dogeon188 <dogcatdog188@gmail.com>
2020-06-08 20:30:53 +0800

    owo

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,26 +1,29 @@
 l = "https://spreadsheets.google.com/feeds/cells/12kGrnf4m7rVOB3G-QDAU8UfhMSkSbxzEdLsOm7Pcm4E/1/public/values?alt=json";
 
-d = $$id("blogs");
+d = $("#blogs");
 j = JSON.parse(G(l)).feed.entry;
-d.innerHTML = "";
+d.empty();
 
 for (o of j) {
   i = o.gs$cell;
   if (i.row == '1') continue;
   switch (i.col) {
     case '1':
-      a = $ce("A");
-      v = $ce("DIV");
-      v.setAttribute("class", "blog-post");
-      h = $ce("H4");
-      h.innerHTML = i.$t;
-      $ac(v,h); $ac(a,v); $ac(d,a);
+      d.append($(`<a><div class="blog-post"><h4>${i.$t}</h4></div></a>`));
+      // a = $ce("A");
+      // v = $ce("DIV");
+      // v.class = "blog-post";
+      // h = $ce("H4");
+      // h.innerHTML = i.$t;
+      // $ac(v,h); $ac(a,v); $ac(d,a);
       break;
     case '2':
-      d.lastChild.setAttribute("href", i.$t);
+      // d.lastChild.href = i.$t;
+      d.children().last().children().first().attr('href', i.$t);
       break;
     case '3':
-      $ac(d.lastChild.firstChild,$ct(i.$t));
+      d.children().last().children().first().append(i.$t);
+      // $ac(d.lastChild.firstChild,$ct(i.$t));
       break;
   }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -5,25 +5,20 @@ l2 = "12kGrnf4m7rVOB3G-QDAU8UfhMSkSbxzEdLsOm7Pcm4E"
 d = $("#vids");
 j = JSON.parse(G(l.f(l1))).feed.entry;
 c = 0;
-d.innerHTML = "";
+d.empty();
 
 for (o of j) {
   if (c >= 4) break;
   i = o.gs$cell.$t;
   if (i.slice(0,2) == "//") continue;
-  t = $ce("IMG");
-  a = $ce("A");
-  t.src = "https://img.youtube.com/vi/{0}/mqdefault.jpg".f(i);
-  t.class = "video-thumbnail";
-  a.href = "https://youtu.be/{0}".f(i);
-  $ac(a,t); $ac(d,a);
+  d.append($(`<a href="https://youtu.be/${i}"><img class="video-thumbnail" src="https://img.youtube.com/vi/${i}/mqdefault.jpg"></a>`));
   c++;
 }
 
 d = $("#blogs");
 j = JSON.parse(G(l.f(l2))).feed.entry;
 c = 0;
-d.innerHTML = "";
+d.empty();
 
 for (o of j) {
   if (c >= 9) {break;}
@@ -31,19 +26,13 @@ for (o of j) {
   if (i.row == '1') continue;
   switch (i.col) {
     case '1':
-      ta = $ce("A");
-      tdiv = $ce("DIV");
-      tdiv.class = "blog-post";
-      th = $ce("H4");
-      th.innerHTML = i.$t;
-      $ac(tdiv,th); $ac(ta,tdiv); $ac(d,ta);
+      d.append($(`<a><div class=blog-post><h4>${i.$t}</h4></div></a>`));
       break;
     case '2':
-      d.lastChild.href = i.$t;
+      d.children().last().attr('href', i.$t);
       break;
     case '3':
-      tc = $ct(i.$t);
-      $ac(d.lastChild.firstChild,tc);
+      d.children().last().children().first().append(i.$t);
       break;
   }
   c++;
